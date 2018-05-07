009b94766edbab7f9d839f3fae855f9e4fa0ecef
Souradeep "Gogol" Bhattacharya <sbhattacharya@bender.engr.ucr.edu>
2018-05-07 13:49:46 -0700

    Added in lab 3

---
+++ Lab3/kernel.cu
@@ -0,0 +1,65 @@
+/******************************************************************************
+ *cr
+ *cr            (C) Copyright 2010 The Board of Trustees of the
+ *cr                        University of Illinois
+ *cr                         All Rights Reserved
+ *cr
+ ******************************************************************************/
+
+// Define your kernels in this file you may use more than one kernel if you
+// need to
+
+// INSERT KERNEL(S) HERE
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/******************************************************************************
+Setup and invoke your kernel(s) in this function. You may also allocate more
+GPU memory if you need to
+*******************************************************************************/
+void histogram(unsigned int* input, uint8_t* bins, unsigned int num_elements,
+        unsigned int num_bins) {
+
+    // INSERT CODE HERE
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
